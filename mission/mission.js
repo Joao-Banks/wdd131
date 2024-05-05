6dc2db3fb283e65a8a387d184dc573d975935fa1
Joao-Banks <ban23008@byui.edu>
2024-05-04 23:26:25 -0600

    added a dark theme functionality to the page by applying javascript concepts.

---
+++ mission/mission.js
@@ -0,0 +1,24 @@
+const themeSelector = document.getElementById("theme_selector");
+
+function changeTheme() {
+
+const chosenTheme = themeSelector.value;
+
+if (chosenTheme === "dark") {
+
+    document.body.classList.add("dark");
+    
+    document.getElementById("logo").src = "university_logo_dark.webp";
+
+}
+
+else {
+
+    document.body.classList.remove("dark");
+
+    document.getElementById("logo").src = "university_logo.webp";
+
+}
+}
+
+themeSelector.addEventListener("change", changeTheme);
